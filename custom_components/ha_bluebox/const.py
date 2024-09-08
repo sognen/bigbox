982bf39a47ce50643b62aa23fb1e7815650c8a6d
sognen <sognen@hotmail.com>
2024-09-08 15:39:41 +0000

    Adding api to const

--- custom_components/ha_bluebox/const.py
+++
@@ -1,8 +0,0 @@
-"""Constants for ha_bigbox."""
-
-from logging import Logger, getLogger
-
-LOGGER: Logger = getLogger(__package__)
-
-DOMAIN = "ha_bigbox"
-ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
